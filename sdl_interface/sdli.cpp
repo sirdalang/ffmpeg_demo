01573d5c62a742900f13a24167084f1c16275bd7
sirdalang <1461021702@qq.com>
2022-08-26 01:38:01 +0800

    tmp update, sdli

--- sdl_interface/sdli.cpp
+++ sdl_interface/sdli.cpp
@@ -4,21 +4,28 @@
 #include <SDL.h>
 
 #include "log.hpp"
+#include "ffplay_demo.hpp"
+
+NAMESPACE_FFMPEG_DEMO_BEGIN
 
 struct SDLI::InnerData
 {
+    std::string filename;
+
     std::string window_name;
 
     SDL_Window *window{nullptr};
     SDL_Renderer *renderer{nullptr};
 
+    std::shared_ptr<FFPlayDemo> player;
 };
 
-SDLI::SDLI()
+SDLI::SDLI(const std::string &filename)
 {
     data_ = std::make_shared<InnerData>();
 
     data_->window_name = std::string("ffplay_demo");
+    data_->filename = filename;
 }
 
 SDLI::~SDLI()
@@ -31,6 +38,7 @@ int SDLI::init()
     auto &window = data_->window;
     auto &window_name = data_->window_name;
     auto &renderer = data_->renderer;
+    auto &player = data_->player;
 
     if (nullptr != window)
     {
@@ -68,7 +76,16 @@ int SDLI::init()
     if (nullptr == renderer)
     {
         xerror("create renderer failed");
+        return -1;
     }
+
+    if (player)
+    {
+        xerror("player is not null");
+        return -1;
+    }
+
+    player = std::make_shared<FFPlayDemo>();
 
     return 0;
 }
@@ -95,3 +112,5 @@ int SDLI::exec()
     
     return 0;
 }
+
+NAMESPACE_FFMPEG_DEMO_END
